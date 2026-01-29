9c7ff6f600e53d73b09c3c8df8d62717fd7bb961
isuruchathuran <isuruchathuran24@gmail.com>
2026-01-29 08:37:13 +0530

    Toaster - position 🚀

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { Toaster } from "react-hot-toast";
 export default function App() {
   return (
     <div className="w-full h-screen bg-primary text-secondary">
-      <Toaster position="top-center"/>
+      <Toaster position="top-right"/>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/admin/*" element={<AdminPage />} />
